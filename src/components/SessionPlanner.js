0bc73a05ef4e4a23a6cb1f3fdbd006e8f9655251
Mikołaj Tkaczyk <mikopar@gmail.com>
2023-03-07 11:03:57 +0100

    Copy change

--- src/components/SessionPlanner.js
+++ src/components/SessionPlanner.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-const SessionPlanner = ({ trainingSession, setTrainingSession, availableDogs, setAvailableDogs, title, enterID, copy }) => {
+const SessionPlanner = ({ trainingSession, setTrainingSession, availableDogs, setAvailableDogs, title, enterID, copyAll, copyOne }) => {
 
     const [previousValue, setPreviousValue] = useState('');
 
@@ -118,6 +118,15 @@ const SessionPlanner = ({ trainingSession, setTrainingSession, availableDogs, se
                                                 </svg>
 
                                             </button> : null}
+                                    {copyOne ?
+                                        <button onClick={(e) => { copyOne(v) }} className="button ml-2 my-1">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="icon">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
+                                            </svg>
+
+
+                                        </button> : null
+                                    }
                                 </div>
                             </div>
                             {i !== trainingSession.length - 1 ?
@@ -131,8 +140,8 @@ const SessionPlanner = ({ trainingSession, setTrainingSession, availableDogs, se
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
                     </button>
-                    {copy && trainingSession.length > 0 ?
-                        <button onClick={copy} className={`button has-text-weight-semibold`}>
+                    {copyAll && trainingSession.length > 0 ?
+                        <button onClick={copyAll} className={`button has-text-weight-semibold`}>
                             <span className="has-text-weight-semibold">Kopiuj na kolejne wejście</span>
                         </button> : null}
                 </div>
